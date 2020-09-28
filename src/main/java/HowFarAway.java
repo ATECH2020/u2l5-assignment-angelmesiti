cab0f506cebbb61cdc04af4644d8207e6433a6c6
Angel Mesiti <angel.233622@nv.ccsd.net>
2020-09-28 07:01:30 +0000

    I put the variables in the wrong order

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -7,18 +7,18 @@ public class HowFarAway
       Scanner in = new Scanner(System.in);// gets input from user
       
       double lat1; // starting lat
-      double lat2; // ending lat
       double long1; // starting long
+      double lat2; // ending lat
       double long2;// ending long
 
       System.out.print("Enter the latitude of the starting location: ");
       lat1 = in.nextDouble(); //inputs starting lat from user
 
-      System.out.print("Enter the latitude of the ending location: ");
-      lat2 = in.nextDouble(); //inputs ending lat from user
-
       System.out.print("Enter the longitude of the starting location: ");
       long1 = in.nextDouble(); //inputs starting long from user
+
+      System.out.print("Enter the latitude of the ending location: ");
+      lat2 = in.nextDouble(); //inputs ending lat from user
 
       System.out.print("Enter the longitude of the ending location: ");
       long2 = in.nextDouble(); //inputs ending long from user
